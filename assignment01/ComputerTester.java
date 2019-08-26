0c56267bc8925502ff551e2de61ca50663458beb
rutvik2007 <rutvik20007@gmail.com>
2019-08-25 22:35:47 -0400

    computer tester added

---
+++ assignment01/ComputerTester.java
@@ -0,0 +1,34 @@
+package assignment01;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+public class ComputerTester {
+	public static void main(String[] args) {
+		Computer comp1=new Computer("Dell","i7",8,512,true,950);
+		Computer comp2=new Computer("Lenovo","i7",16,3072,false,1500);
+		Computer comp3=new Computer("Acer","i5",12,2048,true,1100);
+		Computer comp4=new Computer("HP","i3",8,512,false,400);
+		System.out.println(comp1);
+		System.out.println(comp2);
+		System.out.println(comp3);
+		System.out.println(comp4);
+	//combinations of disk size (two more than a terabyte and two less)
+	//and for each make one an SSD drive and the other not.
+	//Then call System.out.println(comp); for each "comp" object
+	//and observe that there are small differences in how the computer
+	//information prints out.
+		try(PrintWriter output =new PrintWriter(new FileOutputStream(
+			    new File("output.txt"), true /* true means append to file */))) {
+			output.println("\nTESTS FOR Computer.java:");
+
+		//Copy all your lines above, add an extra "2" to every variable name
+		// and replace every System.out.print or System.out.println
+		// by output.print or output.println
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+
